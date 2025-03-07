5783e5b842b5652c9c7ae685383c78a9c2375bb2
Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
2025-03-08 02:11:47 +0300

    Update HCS-1 File Service tests
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>

--- tests/integration/test_hcs_file_service.py
+++ tests/integration/test_hcs_file_service.py
@@ -14,10 +14,19 @@ from .conftest import OPERATOR_KEY_DER
 @pytest.mark.asyncio(loop_scope="session")
 class TestHcsFileService:
     @pytest.mark.parametrize(
-        "test_file_path, expected_chunks_count",
-        [("./tests/test_data/test_file.txt", 1), ("./tests/test_data/test_file_large.txt", 6)],
+        "test_file_path, expected_chunks_count, expected_hash",
+        [
+            ("./tests/test_data/test_file.txt", 1, "48457c7f847ac24fc2419106eae4bb62cf90e25cfb4de0e334fb57df4f7aa4c4"),
+            (
+                "./tests/test_data/test_file_large.txt",
+                6,
+                "96ec5e6330a0850610a81b24b103b1b61ccf38884f5453427229c27e818f42ef",
+            ),
+        ],
     )
-    async def test_submit_file(self, test_file_path: str, expected_chunks_count: int, client: Client):
+    async def test_submit_and_resolve_file(
+        self, test_file_path: str, expected_chunks_count: int, expected_hash: str, client: Client
+    ):
         service = HcsFileService(client)
         file_payload = Path(test_file_path).read_bytes()
 
@@ -32,20 +41,9 @@ class TestHcsFileService:
         assert topic_info.memo == f"{sha256(file_payload).hexdigest()}:zstd:base64"
         assert len(topic_messages) == expected_chunks_count
 
-    @pytest.mark.parametrize(
-        "topic_id, expected_hash",
-        [
-            ("0.0.5123926", "ea4d17b8c0cf44c215ade6b4ad36832672ea3188b1dad12b68c2472dfbcdeff1"),
-            ("0.0.5123993", "dce9e97491cb7bbaeb6f1af9c236a62f5b6f3f4c07952b5431daa52ec58fbc0b"),
-        ],
-    )
-    async def test_resolve_file(self, topic_id: str, expected_hash: str, client: Client):
-        service = HcsFileService(client)
-
         resolved_payload = await service.resolve_file(topic_id)
         assert resolved_payload
 
-        topic_info = await execute_hcs_query_async(TopicInfoQuery(topic_id=TopicId.from_string(topic_id)), client)
         topic_file_hash, _, _ = topic_info.memo.split(":")
 
         assert topic_file_hash == sha256(resolved_payload).hexdigest()
